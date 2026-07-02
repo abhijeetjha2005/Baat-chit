793ac43be65e3fef9aac808758b7cdc62310a5d8
abhijeetjha2005 <abhijeethoshiyar100@gmail.com>
2026-07-02 17:55:42 +0530

    Your descriptive commit message here

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -54,6 +54,28 @@ const LandingPage = () => {
         <h1 className='text-5xl font-bold text-white mb-3 tracking-tight'>बात-चीत</h1>
         <p className='text-zinc-400 text-lg mb-12'>Connect with friends instantly</p>
         {/* login */}
+        <div className='bg-zic-900/80 backdrop-blur-xl border border-zinc-700 rounded-3xl p-8'>
+        <h2 className='text-2xl text-white font-semibold mb-6'>Sign In</h2>
+        <form className='space-y-5'>
+          <input type="email"
+          placeholder='E-mail'
+          className='w-full px-5 py-4 bg-zinc-800 border border-zinc-700 rounded-2xl text-white focus:outline-none focus:border-emerald-500'
+          />
+          <input type="password"
+           placeholder='Password'
+           className='w-full px-5 py-4 bg-zinc-800 border border-zinc-700 text-white font-semibold rounded-2xl transition'
+          />
+          <button 
+          className='w-full py-4 bg-emerald-500 hover:bg-emerald-600 text-white font-semibold rounded-2xl transition'>
+            Sign In
+          </button>
+        </form>
+        {/* sign up */}
+        <p className='text-zinc-400 mt-6 text-sm'>Don't have an account?{' '}</p>
+          <a href="#" className='text-emerald-400 hover:text-emerald-500 font-medium'>
+            Sign Up
+          </a>
+        </div>
       </div>
 
     </div>
