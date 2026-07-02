d1bcd9cd35bc173b8ff377a5b4c0df285d9cbe33
abhijeetjha2005 <abhijeethoshiyar100@gmail.com>
2026-07-02 17:25:12 +0530

    Your descriptive commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import LandingPage from './pages/LandingPage'
 
 const App = () => {
   return (
-    <div>app</div>
+    <div className="App"> 
+<LandingPage/>
+    </div>
   )
 }
 
