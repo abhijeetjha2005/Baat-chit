bef48b31d224da0e763771be2d65546a834a48bd
abhijeetjha2005 <abhijeethoshiyar100@gmail.com>
2026-07-07 20:03:30 +0530

    Add registerUser authentication controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,6 +2,35 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
 
+// register user
+const registerUser= async(req,res)=>{
+  try{
+    let{name,phoneNumber,email,password,confirmPassword}=req.body;
+    // validation
+    if(!name||!phoneNumber||!email||!password||!confirmPassword){
+     return res.status(400).json({ success: false, message: "All fields are required" }); 
+    }
+    if(password!==confirmPassword){
+      return res.status(400).json({ success: false, message: "Passwords do not match" });
+    }
+    // check if user exists
+    const existingUser= await User.findOne({$or:[{email},{phoneNumber}]})
+    // create new user
+    const user = await User.create({ name, phoneNumber, email, password });
+    res.status(201).json({
+      success:true,
+      message:"Registration successful",
+      user:{id:user._id,name:user.name,email:user.email}
+    })
+  }catch(error){
+    console.error(error);
+    res.status(500).json({success: false, message: "Server error"})
+    
+  }
+}
+
+
+// login code
 const login = async (req, res) => {
      try{
       let {email,password}=req.body
@@ -36,4 +65,6 @@ return res.status(200).json({
       return res.status(500).json({ message: "An unexpected error occurred." });
      }
 }
-module.exports = { login };
+
+
+module.exports = { registerUser,login };
