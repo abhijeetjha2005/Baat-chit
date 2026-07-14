eb80009ea43445d315a712a2ed26dcf2568d83a1
abhijeetjha2005 <abhijeethoshiyar100@gmail.com>
2026-07-14 21:53:33 +0530

    Connect frontend with backend

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from "axios"
 import { useNavigate } from "react-router-dom";
 import Logo from "../components/Logo";
 
 const LandingPage = () => {
   const navigate = useNavigate();
+
+const[formData,setFormData]=useState({
+email:"",
+password:""
+})
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+const handleChange =(e)=>{
+  setFormData({
+    ...formData,
+    [e.target.name]:e.target.value,
+})
+}
+
+const handleSubmit= async(e)=>{
+  e.preventDefault();
+  setError("");
+  try{
+setLoading(true)
+const response=await axios.post
+(
+  "http://localhost:3000/api/auth/login",
+{
+email:formData.email,
+password:formData.password,
+  }
+)
+if (response.data.message === "Login successful") {
+      navigate("/chat");
+}
+  }catch(error){
+    setError(
+      error.response?.data?.message||"login Failed"
+    )
+  }finally {
+    setLoading(false);
+  }
+}
+
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-zinc-950 via-black to-zinc-950 flex items-center justify-center px-4 py-8 sm:px-6">
@@ -30,9 +72,12 @@ const LandingPage = () => {
             Sign In
           </h2>
 
-          <form className="space-y-5">
+          <form onSubmit={handleSubmit} className="space-y-5">
             <input
               type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
               placeholder="Email address"
               className="w-full px-5 py-4 bg-zinc-800 border border-zinc-700 rounded-2xl 
                          text-white placeholder-zinc-500 focus:outline-none 
@@ -41,18 +86,28 @@ const LandingPage = () => {
 
             <input
               type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
               placeholder="Password"
               className="w-full px-5 py-4 bg-zinc-800 border border-zinc-700 rounded-2xl 
                          text-white placeholder-zinc-500 focus:outline-none 
                          focus:border-emerald-500 focus:ring-1 focus:ring-emerald-500 transition-all"
             />
 
+
+{error && (
+  <p className="text-red-500 text-sm text-center">
+    {error}
+  </p>
+)}
             <button
               type="submit"
+                disabled={loading}
               className="w-full py-4 bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 
                          text-white font-semibold rounded-2xl transition-all duration-200 text-base"
             >
-              Sign In
+           {loading ? "Signing In..." : "Sign In"}
             </button>
           </form>
 
